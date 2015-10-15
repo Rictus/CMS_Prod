5670ca886f51c772d8b7320a2e845d2af17b07f3
Rictus <contact.dylanleroux@gmail.com>
2015-10-15 18:27:55 +0200

    footer

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,20 +1,56 @@
-		<div class="wrap">
-	            <footer id="bottom">
-	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+<footer id="bottom">
+    <ul>
+        <?php if (has_menu_items()): ?>
+        <div class="row">
 
-	                <ul role="navigation">
-	                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
-	                    <?php if(twitter_account()): ?>
-	                    <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
-	                    <?php endif; ?>
+            <ul>
+                <?php while (menu_items()): ?>
+                    <li <?php echo(menu_active() ? 'class="active"' : ''); ?>>
+                        <a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
+                            <?php echo menu_name(); ?>
+                        </a>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
 
-	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
-
-	                    <li><a href="/" title="Return to my website.">Home</a></li>
-	                </ul>
-	            </footer>
-
-	        </div>
         </div>
-    </body>
+        <div class="row">
+            <ul>
+                <li>
+                    <a href="">Info éditeur</a>
+                </li>
+                <li>
+                    <a href="">Mentions légagles</a>
+                </li>
+                <li>
+                    <a href="">Crédits</a>
+                </li>
+            </ul>
+        </div>
+        <div class="row">
+            <ul>
+                <li>
+                    <a href="">Plan du site</a>
+                </li>
+                <li>
+                    <a href="">Réseaux sociaux</a>
+                </li>
+            </ul>
+
+        </div>
+        <!-- /*
+        <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
+        <?php if (twitter_account()): ?>
+            <li><a href="<?php echo twitter_url(); ?>">@<?php echo twitter_account(); ?></a></li>
+        <?php endif; ?>
+        <?php endif; ?>
+
+        <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin">Home</a></li>
+        //*/ -->
+    </ul>
+</footer>
+</div>
+
+</div>
+</body>
 </html>
